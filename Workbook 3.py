36e5cb2f7674169f7e412d62c03454c91d32facd
skilloran <54560943+skilloran@users.noreply.github.com>
2019-09-16 13:03:42 -0400

    Add files via upload

---
+++ Workbook 3.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+
+names = ['Bob', 'Jessica', 'Mary', 'Jon', 'Mel']
+grades = [76, -2, 77, 78, 101]
+
+GradeList = zip(names,grades)
+df = pd.DataFrame(data = GradeList,
+                 columns = ['Names', 'Grades'])
+df
+
+
+# In[10]:
+
+
+df.loc [(df['Grades'] <= 0, 'Grades')] = 0
+df
+
+
+# In[ ]:
+
+
+
+
